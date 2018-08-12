5d532fdc6c0ff41064d5637e0216129af1b78db5
MountainRains <lushanyu1996@gmail.com>
2018-08-12 21:23:41 +0800

    task

---
+++ 多线程处理/Task/Thread/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace Thread_Learn
+{
+    class Program
+    {
+        public const int Repetitions = 10000;
+        static void Main(string[] args)
+        {
+            Task task = Task.Run((Action) DoWork);
+            for (int i = 0; i < Repetitions; i++)
+            {
+                Console.Write("-");
+            }
+            task.Wait();
+        }
+
+        private static void DoWork()
+        {
+            for(int count =0; count < Repetitions; count++)
+            {
+                Console.Write("+");
+            }
+        }
+    }
+}
