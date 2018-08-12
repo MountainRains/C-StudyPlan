8429a401a982502da46d3130482e5c8b0ad8fb53
MountainRains <lushanyu1996@gmail.com>
2018-08-12 20:45:12 +0800

    最基本的多线程实例，在Thread方法中传入了一个无返回委托

--- 多线程处理/Thread/Thread/Program.cs
+++ 多线程处理/Thread/Thread/Program.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Thread
+using System.Threading;
+namespace Thread_Learn
 {
     class Program
     {
+        public const int Repetitions = 1000;
         static void Main(string[] args)
         {
+            ThreadStart threadStart = DoWork;
+            //Thread thread = new Thread(threadStart);
+            Thread thread = new Thread(DoWork);
+            thread.Start();
+            //thread.Join();
+            for (int i = 0; i < Repetitions; i++)
+            {
+                Console.Write("-");
+            }
+            thread.Join();
+        }
+
+        private static void DoWork()
+        {
+            for(int count =0; count < Repetitions; count++)
+            {
+                Console.Write('+');
+            }
         }
     }
 }
